29857b99d4e9fb33f7919b6275a1597e88e3a712
root <user@example.com>
2024-04-29 09:39:46 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zaesn/ggapi
+
+go 1.21.5
---
+++ src/hello.go
@@ -0,0 +1,14 @@
+/**
+ * hello
+ * @Name: hello
+ * @Version: 1.0.0
+ * @Author: coder.yee
+ * @Date: 2024/4/28 18:17
+ * @Description:
+ */
+
+package src
+
+func Hello() string {
+	return "Hello my test."
+}
